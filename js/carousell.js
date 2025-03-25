e7c797c57af251b6eb007b9becf4ed19554e38de
João Victor da Encarnação Cunha <158203459+xeilongue@users.noreply.github.com>
2025-03-25 11:13:57 -0300

    Add files via upload

---
+++ js/carousell.js
@@ -0,0 +1,68 @@
+// carouselArr.push(new Carousel("imagem_1.jpg","Esta é a nova Ranger Ford 2022. Verifique novidades.","lancamento.html"));
+// carouselArr.push(new Carousel("imagem_2.jpg","Ford a nossa história","#"));
+// carouselArr.push(new Carousel("imagem_3.jpg","Nova Ford Bronco Sport 2022","lancamento.html"));
+
+class Carousel {
+
+    constructor(content) {
+        this.img = document.getElementById('img-carousel')
+        this.title = document.getElementById('title-carousel')
+        this.url = document.getElementById('href-carousel')
+        this.content = content
+        this.index = 0
+    }
+
+    start(interval) {
+
+        this.updateElements()
+        this.next()
+
+        setInterval(() => {
+
+            this.updateElements()
+            this.next()
+            
+        }, interval)
+
+    }
+
+    next() {
+
+        if (this.index === content.length-1) {
+            this.index = 0
+        }
+        else {
+            this.index++
+        }
+
+    }
+
+    updateElements() {
+        this.img.src = this.content[this.index].img
+        this.title.innerHTML = this.content[this.index].title
+        this.url.href = this.content[this.index].url
+    }
+
+}
+
+const content = [
+    {
+        img: "./img/imagem_1.jpg",
+        title: "Esta é a nova Ranger Ford 2022. Verifique novidades.",
+        url: "lancamento.html"
+    },
+    {
+        img: "./img/imagem_2.jpg",
+        title: "Ford a nossa história",
+        url: "lancamento.html"
+    },
+    {
+        img: "./img/imagem_3.jpg",
+        title: "Nova Ford Bronco Sport 2022",
+        url: "lancamento.html"
+    }
+]
+
+const carousel = new Carousel(content)
+
+carousel.start(2000)
