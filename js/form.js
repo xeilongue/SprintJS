5a5f8fbb918c91e44408d9bcdc2b3ba1a9714ec7
João Victor da Encarnação Cunha <joao.v.cunha6@ba.estudante.senai.br>
2025-04-08 10:51:37 -0300

    botao apenas quando o termo estiver ativado

--- js/form.js
+++ js/form.js
@@ -2,19 +2,32 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, contatoPreferido, mensagem, tipoMensagem) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contatoPreferido = contatoPreferido;
+        this.mensagem = mensagem;
+        this.tipoMensagem = tipoMensagem;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+    let data = new contato(form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("cpf").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value);
+
+        validarBotao()
 }
+
+const botaoctt = document.getElementById('botaocontato')
+const checkbox = document.getElementById('checkboxtermos')
 
 function Enviar() {
 
@@ -23,5 +36,18 @@ function Enviar() {
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
+    
+}
 
+checkbox.addEventListener("change", validarBotao)
+
+function validarBotao() {
+    if (checkbox.checked) {
+        botaoctt.disabled = false
+        botaoctt.style.opacity = 1
+    }
+    else {
+        botaoctt.disabled = true
+        botaoctt.style.opacity = 0.3
+    }
 }
